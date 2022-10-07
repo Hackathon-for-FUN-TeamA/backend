78ff95b839a9f8ae966b3c945a9b2d0ecc2b0c38
funera1 <60760935+funera1@users.noreply.github.com>
2022-10-07 21:04:38 +0900

    login機能追加

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,25 +15,6 @@ func main() {
 		})
 	})
 
-	r.POST("/test/user/create", func(c *gin.Context) {
-		username := c.PostForm("username")
-		password := c.PostForm("password")
-
-		if username == "" || password == "" {
-			c.JSON(400, gin.H{
-				"message":  "query string is null",
-				"username": username,
-				"password": password,
-			})
-		} else {
-			c.JSON(200, gin.H{
-				"message":  "Created",
-				"username": username,
-				"password": password,
-			})
-		}
-	})
-
 	r.POST("/user/create", func(c *gin.Context) {
 		username := c.PostForm("username")
 		password := c.PostForm("password")
@@ -55,6 +36,29 @@ func main() {
 			})
 		}
 
+	})
+
+	r.POST("/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		if username == "" || password == "" {
+			c.JSON(400, gin.H{
+				"message": "query string is null",
+			})
+		}
+
+		// TODO: authとか作って認証できるとよい
+		err := user.Login(username, password)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"message": err,
+			})
+		} else {
+			c.JSON(200, gin.H{
+				"message": "Login",
+			})
+		}
 	})
 
 	r.Run() // 127.0.0.0:8000でサーバを建てる
