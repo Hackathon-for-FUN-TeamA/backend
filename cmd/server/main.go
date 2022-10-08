d168008747c31652f7b624ea0ad7e130c302a65c
funera1 <60760935+funera1@users.noreply.github.com>
2022-10-09 07:24:47 +0900

    test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,6 +16,16 @@ func main() {
 		})
 	})
 
+	// sample
+	r.POST("/throw", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+		c.JSON(200, gin.H{
+			"username": username,
+			"password": password,
+		})
+	})
+
 	// ユーザ作成
 	r.POST("/user/create", func(c *gin.Context) {
 		username := c.PostForm("username")
