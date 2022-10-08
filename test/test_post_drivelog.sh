9e9f4534fce74bf8f219f06e2c72462e5c4cbf30
funera1 <60760935+funera1@users.noreply.github.com>
2022-10-09 04:26:41 +0900

    文字列をカッコで囲んだ

--- test/test_post_drivelog.sh
+++ test/test_post_drivelog.sh
@@ -1,10 +1,9 @@
 endpoint=localhost:8080
 curl -X POST $endpoint/drivelog \
-    -d { \
-      token=sample1, \
-      date="0000-01-01:00:00", \
-      speed=100.00, \
-      acceleration=100.00, \
-      latitude=100.00, \
-      longtitude=100.00, \
-    }
+    -d token='sample1' \
+    -d date="0000-01-01:00:00" \
+    -d speed=100.00 \
+    -d acceleration=100.00 \
+    -d latitude=100.00 \
+    -d longtitude=100.00 \
+    jq .
